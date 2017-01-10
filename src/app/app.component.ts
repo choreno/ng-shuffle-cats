3d0ff3d0165d5ea5addd7e27b11853f54ac3e8b6
THBar <choreno@gmail.com>
2017-01-09 21:51:54 -0500

    refactored codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
-
-
-  tiles: any[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
 
   cards: any[] = [
-    {text: 'Spade A'},
-    {text: 'Spade 2'},
-    {text: 'Spade 3'},
-    {text: 'Spade 4'},
-    {text: 'Spade 5'},
-    {text: 'Spade 6'},
-    {text: 'Spade 7'},
-    {text: 'Spade 8'},
-    {text: 'Spade 9'},
-    {text: 'Spade 10'},
-    {text: 'Spade J'},
-    {text: 'Spade Q'},
-    {text: 'Spade K'},
+    { number: 'ace', type: 'spades' },
+    { number: '2', type: 'spades' },
+    { number: '3', type: 'spades' },
+    { number: '4', type: 'spades' },
+    { number: '5', type: 'spades' },
+    { number: '6', type: 'spades' },
+    { number: '7', type: 'spades' },
+    { number: '8', type: 'spades' },
+    { number: '9', type: 'spades' },
+    { number: '10', type: 'spades' },
+    { number: 'jack', type: 'spades' },
+    { number: 'queen', type: 'spades' },
+    { number: 'king', type: 'spades' },
 
-    {text: 'Diamond A'},
-    {text: 'Diamond 2'},
-    {text: 'Diamond 3'},
+    { number: 'ace', type: 'diamonds' },
+    { number: '2', type: 'diamonds' },
+    { number: '3', type: 'diamonds' },
+    { number: '4', type: 'diamonds' },
+    { number: '5', type: 'diamonds' },
+    { number: '6', type: 'diamonds' },
+    { number: '7', type: 'diamonds' },
+    { number: '8', type: 'diamonds' },
+    { number: '9', type: 'diamonds' },
+    { number: '10', type: 'diamonds' },
+    { number: 'jack', type: 'diamonds' },
+    { number: 'queen', type: 'diamonds' },
+    { number: 'king', type: 'diamonds' },
+
+    { number: 'ace', type: 'hearts' },
+    { number: '2', type: 'hearts' },
+    { number: '3', type: 'hearts' },
+    { number: '4', type: 'hearts' },
+    { number: '5', type: 'hearts' },
+    { number: '6', type: 'hearts' },
+    { number: '7', type: 'hearts' },
+    { number: '8', type: 'hearts' },
+    { number: '9', type: 'hearts' },
+    { number: '10', type: 'hearts' },
+    { number: 'jack', type: 'hearts' },
+    { number: 'queen', type: 'hearts' },
+    { number: 'king', type: 'hearts' },
+
+
+    { number: 'ace', type: 'clubs' },
+    { number: '2', type: 'clubs' },
+    { number: '3', type: 'clubs' },
+    { number: '4', type: 'clubs' },
+    { number: '5', type: 'clubs' },
+    { number: '6', type: 'clubs' },
+    { number: '7', type: 'clubs' },
+    { number: '8', type: 'clubs' },
+    { number: '9', type: 'clubs' },
+    { number: '10', type: 'clubs' },
+    { number: 'jack', type: 'clubs' },
+    { number: 'queen', type: 'clubs' },
+    { number: 'king', type: 'clubs' },
+
+
+
   ]
 
 }
