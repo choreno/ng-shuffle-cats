cbbfa947f145da2dfdde2b743c6f5e07f5ccb419
THBar <choreno@gmail.com>
2017-01-10 17:06:09 -0500

    added probability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { CardService } from './services/card.service';
 import { ICard } from './interfaces/card.interface';
 import { CardStatus } from './enums/card.status';
 
+import { Observable } from 'rxjs/Observable';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,27 +18,34 @@ export class AppComponent {
   cards: ICard[];
   selectedCard: ICard;
   isVisible: boolean = true;
-  possibility: number;
+
+  numberOfCards: number;
+  initialProbability: number ; //users will have the 7 cards each for starting game
+  probability: number;
 
   constructor(private _cardService: CardService) { }
 
 
   toggleCardDisplay(index: number) {
 
+    //For discarded cards
     if (this.cards[index].status === CardStatus.Decked) {
       this.cards[index].isDisplay = !this.cards[index].isDisplay;
-      this.cards[index].status = CardStatus.Discarded; 
+      this.cards[index].status = CardStatus.Discarded;
+
+      this.probability = this.updateProbability(this.numberOfCards--);
+
     }
 
-    var numberOfCards = 0 ; 
-
-  }
+  };
 
   resetCards(): void {
 
     for (var card of this.cards) {
       card.isDisplay = true;
     }
+
+    this.probability = this.initialProbability; 
 
   };
 
@@ -44,12 +54,30 @@ export class AppComponent {
 
     this.selectedCard = card;
 
-  }
+  };
+
+  updateProbability(currentNumberOfCards): number {
+    
+    var numberOfUserCards = 14; //two users will have the 7 cards each for starting game
+    var probability = 100 / (currentNumberOfCards - numberOfUserCards);
+
+    if (currentNumberOfCards == numberOfUserCards || currentNumberOfCards < numberOfUserCards) {
+      probability = 100;
+    }
+
+    return probability;
+  };
 
   ngOnInit() {
+
     this._cardService.getAllCards().subscribe(cards => this.cards = cards,
       err => console.log(err),
-      () => console.log('loading cards, done'))
+      () => {
+        this.numberOfCards = this.cards.length;
+        this.probability = this.updateProbability(this.numberOfCards) ;
+        this.initialProbability = this.probability;         
+      });
+
   }
 
 
